83d6e278ae55c08245eeec3e6b9faf9f6529fb0e
NicoleAgurcia <nicole_agurcia@unitec.edu>
2014-11-02 23:53:08 -0600

    Tarea3

--- main.cpp
+++ main.cpp
@@ -1,36 +1,122 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <windows.h>
+
+
+
 
 using namespace std;
 
 class Archivos{
     public:
        string nombre_archivo;
+       Archivos * sig;
+       Archivos();
+       Archivos(string);
 
-       Archivos(string nombre_archivo){
-           this->nombre_archivo=nombre_archivo;
-       }
+
+
 };
+typedef Archivos * ARC;
+
+Archivos::Archivos(){
+    sig= NULL;
+}
+Archivos::Archivos(string vnombre_archivo){
+    nombre_archivo = vnombre_archivo;
+    sig=NULL;
+
+}
+
 
 list <Archivos*> ArchivosCreados;
 
 
-void creararchivo(string nombre_archivo,Archivos *m){
-    ofstream out(nombre_archivo.c_str(),ios::app|ios::binary);
-    cout << "Archivo creado con exito..." << endl;
-    out.write(m->nombre_archivo.c_str(),100);
-    out.close();
-}
-
-void listararchivos(){
-    while(!ArchivosCreados.empty()){
-            for(int x=0; x < ArchivosCreados.size(); x++){
-                cout << ArchivosCreados.front() << endl;
-            }
-            ArchivosCreados.pop_front();
+//kakaka
+void insartararchivo(ARC &arc, string vnombre_archivo){
+    Archivos *tem; Archivos *new_nodo;
+    new_nodo = new Archivos();
+    new_nodo->nombre_archivo = vnombre_archivo;
+    if(arc == NULL){ arc = new_nodo;}
+    else{
+        tem= arc;
+        while(tem->sig != NULL){
+            tem = tem->sig;
+        }
+        tem->sig = new_nodo;
     }
 }
+
+void listararchivos(Archivos * arc){
+    Archivos * temp;
+    temp = arc;
+    while(temp != NULL){
+        cout << temp->nombre_archivo;
+        if(temp->sig != NULL){
+            cout << endl;
+        }
+        temp = temp->sig;
+    }
+}
+
+//void listarDirectorio(int argc, TCHAR *argv[])
+//{
+//   WIN32_FIND_DATA FindFileData;
+//   HANDLE hFind;
+//
+//   if( argc != 2 )
+//   {
+//      cout << (TEXT("Usage: %s [target_file]\n"), argv[0]);
+//      return;
+//   }
+//
+//   cout << (TEXT("Target file is %s\n"), argv[1]);
+//   hFind = FindFirstFile(argv[1], &FindFileData);
+//   if (hFind == INVALID_HANDLE_VALUE)
+//   {
+//       cout << ("FindFirstFile failed (%d)\n", GetLastError());
+//      return;
+//   }
+//   else
+//   {
+//       cout <<  (TEXT("The first file found is %s\n"),
+//                FindFileData.cFileName);
+//      FindClose(hFind);
+//   }
+//}
+
+
+
+
+
+void creararchivo(string nombre_archivo){
+    Archivos * arc= NULL;
+    ofstream out(nombre_archivo.c_str(),ios::app|ios::binary);
+    if(out.is_open()){
+    cout << "Archivo creado con exito..." << endl;
+    }
+
+
+
+//    for(list<Archivos*>::iterator i = ArchivosCreados.begin(); i!=ArchivosCreados.end(); i++){
+//            out<<(*i)->nombre_archivo<<" ";
+//    }
+//    ArchivosCreados.push_front(new Archivos(nombre_archivo));
+
+    out.close();
+    // m->nombre_archivo = nombre_archivo;
+    //ArchivosCreados.push_front(m);
+}
+
+//void listararchivos(){
+//    //while(!ArchivosCreados.empty()){
+//           for(list<Archivos*>::iterator i = ArchivosCreados.begin(); i!=ArchivosCreados.end(); i++){
+//                cout<<(*i)->nombre_archivo<<endl;
+//           }
+//    //}
+//
+//}
 
 void modificararchivo(string nombre_archivo){
     string line;
@@ -113,6 +199,7 @@ Archivos * getArchivo(){
 int main()
 {
     string op="";
+     Archivos * arc= NULL;
 
 
 string nombre_archivo;
@@ -124,7 +211,8 @@ string nombre_archivo;
 
 
             cin >> nombre_archivo;
-            creararchivo(nombre_archivo,getArchivo());
+            creararchivo(nombre_archivo);
+            insartararchivo(arc,nombre_archivo);
         }
         if(op=="modificararchivo"){
             cin >> nombre_archivo;
@@ -137,7 +225,11 @@ string nombre_archivo;
             //verarchivobin(nombre_archivo);
         }
         if(op=="listararchivos"){
-            //listararchivos();
+
+                listararchivos(arc);
+
+
+
         }
 
 
